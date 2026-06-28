253004c77cd3271c045f51bc3f6b3ea8f9b393c1
amanverma123-prog <amanverma3911@gmail.com>
2026-06-28 17:36:30 +0530

    Add role-based access control

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from students import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/students/')),
     path('admin/', admin.site.urls),
     path('students/', include('students.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='students/login.html'), name='login'),
